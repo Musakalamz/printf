feb7143c654735997bcfa6366eef24ed9612de6f
Musakalamz <ogunsolumusa@gmail.com>
2023-08-25 08:13:44 -0700

    Handles specifier, flags, width, length and precision

--- handlers.c
+++ handlers.c
@@ -10,8 +10,20 @@ unsigned char handle_flags(const char *flag, char *index);
 unsigned char handle_length(const char *modifier, char *index);
 int handle_width(va_list args, const char *modifier, char *index);
 int handle_precision(va_list args, const char *modifier, char *index);
+
+
+/**
+ * handle_specifiers - Matches a conversion specifier with
+ *                     a corresponding conversion function.
+ * specifier: The formag specifier.
+ * @int: integer
+ * @char: character
+ *
+ * Return: If a conversion function is matched - a pointer to the function.
+ *         Otherwise - NULL.
+ */
 unsigned int (*handle_specifiers(const char *specifier))(va_list, buffer_t *,
-		unsigned char, int, int, unsigned char);
+	unsigned char, int, int, unsigned char);
 
 /**
  * handle_flags - Matches flags with corresponding values.
@@ -166,7 +178,9 @@ int handle_precision(va_list args, const char *modifier, char *index)
 /**
  * handle_specifiers - Matches a conversion specifier with
  *                     a corresponding conversion function.
- * @specifier: A pointer to a potential conversion specifier.
+ * specifier: A pointer to a potential conversion specifier.
+ * @int: integer
+ * @char: character
  *
  * Return: If a conversion function is matched - a pointer to the function.
  *         Otherwise - NULL.
